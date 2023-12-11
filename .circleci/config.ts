3a8ebd02dca3abb0ce83a63df7d5ad559f04366c
Gordon Syme <gordonsyme@gmail.com>
2023-12-11 11:49:13 +0000

    Just run a script

--- .circleci/config.ts
+++ .circleci/config.ts
@@ -1,12 +1,3 @@
-import { readConfigFile } from "../src/lib/Config";
-import { Workflow, BuildJob } from "../src/lib/Components/Workflow";
+import {Workflow} from "../src/lib/Components/Workflow";
 
-const buildConfig = readConfigFile(".circleci/test-config.yml")
-  .getJobConfig("build");
-
-const workflow = new Workflow("breaking-workspaces")
-  .addJob(new BuildJob("build")
-    .withConfig(buildConfig!)
-    .withContext("org-global"));
-
-workflow.generateConfig();
+Workflow.default("build-script.sh").generateConfig();
